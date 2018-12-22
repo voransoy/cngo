f8f3c65f6e538386e502e85f6853c139282a7d52
Volkan Oransoy <voransoy@gmail.com>
2018-12-22 16:55:10 +0300

    testing

--- cngo.go
+++ cngo.go
@@ -10,7 +10,7 @@ func main() {
 	r := gin.Default()
 
 	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{ "status" : "OK" })
+		c.JSON(http.StatusOK, gin.H{ "status" : "OKAY" })
 	})
 
 	r.GET("/t/:host", testWebsite)
